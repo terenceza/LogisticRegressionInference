bdb80b69f829a04c12ab783bf03b958e01137174
terenceza <158005283+terenceza@users.noreply.github.com>
2025-04-30 17:36:23 -0600

    Update LogRegressionInfHost.cpp

--- LogRegressionInfHost.cpp
+++ LogRegressionInfHost.cpp
@@ -1,21 +1,8 @@
 /******************************************************************************
-* Copyright (C) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
-* SPDX-License-Identifier: MIT
+* Sensecoding.com free license
+* Run on Vitis 
 ******************************************************************************/
-/*
- * helloworld.c: simple test application
- *
- * This application configures UART 16550 to baud rate 9600.
- * PS7 UART (Zynq) is not initialized by this application, since
- * bootrom/bsp configures it to baud rate 115200
- *
- * ------------------------------------------------
- * | UART TYPE   BAUD RATE                        |
- * ------------------------------------------------
- *   uartns550   9600
- *   uartlite    Configurable only in HW design
- *   ps7_uart    115200 (configured by bootrom/bsp)
- */
+
 #include <stdio.h>
 #include "platform.h"
 #include "xil_printf.h"
@@ -23,20 +10,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "LogRegression.h"
+#include "LogRegressionInf.h"
 #include "xscutimer.h"
 #include <time.h>
-
-#define MAX_LINE_LENGTH 100
-#define DDR_DATA_BASE_ADDR_IN       0x0000000000000000
-#define DDR_PREDICT_BASE_ADDR       DDR_DATA_BASE_ADDR_IN + 256
-#define DDR_WEIGHTS_BASE_ADDR       DDR_DATA_BASE_ADDR_IN + 1024
 
 XLogregressioninf LogRegInst;
 XLogregressioninf_Config Cfg;
 
 void LogRegressionInfInit(XLogregressioninf* Inst, XLogregressioninf_Config* Cfg)
-//,                        u64* input_addr, u64* output_addr)
 {
 	if ( XLogregressioninf_Initialize(Inst, XPAR_XLOGREGRESSIONINF_0_DEVICE_ID) != XST_SUCCESS)
 	{
@@ -47,160 +28,131 @@ void LogRegressionInfInit(XLogregressioninf* Inst, XLogregressioninf_Config* Cfg
 	    xil_printf("XLogregressioninf_Initialize success \n");
     }
 
-    //XAccelconvoluteaxi_EnableAutoRestart(Inst);
 	XLogregressioninf_InterruptGlobalDisable(Inst);
 	XLogregressioninf_InterruptDisable(Inst, 1);
+}
+
+void NormalizeInputs()
+{
+    int numElements = sizeof(testData) / sizeof(testData[0]);
 }
 
 void WriteDataToBus()
 {
     int numElements = sizeof(testData) / sizeof(testData[0]);
 
-    xil_printf("Writing float data to FPGA registers, numelements = %d\n", numElements);
+    xil_printf("Writing float input data to DDR, numElements = %d\n", numElements);
 
-    //for (int i = 0; i < numElements;)
-    int i = 0; // data index in data table
-    int k = 0; // data index in memory
-    int j = 0; // label index in memory
+    int32_t* ddr_ptr = (int32_t*)DDR_DATA_BASE_ADDR_IN; 
 
-    for (i = 0 ; i < numElements; ++i)
+    // Write the data
+    for (int i = 0; i < numElements; ++i)
     {
-        uint32_t data32 = 0;
-        memcpy(&data32, &testData[i], sizeof(float));
+        int32_t ival;
+        memcpy(&ival, &testData[i], sizeof(int32_t));
+        XLogregressioninf_WriteReg(DDR_DATA_BASE_ADDR_IN, i*4, ival);
 
-        XLogregressioninf_WriteReg(DDR_DATA_BASE_ADDR_IN, i*4, data32);
+        printf( " 0x%x ", ival);
     }
 
-    // for (i = 0 ; i < numElements; ++i)
-    // {
-    //     uint32_t data32 = 0;
-    //     float    floatValue;
-
-    //     data32 = XLogregressioninf_ReadReg(DDR_DATA_BASE_ADDR_IN, i*4);
-    //     memcpy(&floatValue, &data32, sizeof(float));
-    //     printf("data32: %f\n", floatValue);
-
-    // }
-
     xil_printf("Finished writing data.\n");
+
+    // Read back and verify
+    xil_printf("Reading back data from DDR for verification:\n");
+
+    for (int i = 0; i < numElements; ++i)
+    {
+        int32_t val = XLogregressioninf_ReadReg(DDR_DATA_BASE_ADDR_IN, i*4);
+        xil_printf("Read: 0x%x\n",  val);
+    }
+
 }
 
 void WriteWeightsToBus()
 {
-    int NumFeatures = 4;
-    int i = 0; // data index in data table
+    int numElements = sizeof(Weights) / sizeof(Weights[0]);
 
-    while (i < NumFeatures)
+    xil_printf("Writing float weights data to DDR, numElements = %d\n", numElements);
+
+    int32_t* ddr_ptr = (int32_t*)DDR_WEIGHTS_BASE_ADDR; 
+
+    // Write the data
+    for (int i = 0; i < numElements; ++i)
     {
-        uint32_t data32 = 0;
-        memcpy(&data32, &Weights[i], sizeof(float));
-
-        XLogregressioninf_WriteReg(DDR_WEIGHTS_BASE_ADDR, i*4, data32);
-        ++i;
+        
+        int32_t ival;
+        memcpy(&ival, &Weights[i], sizeof(int32_t));
+        XLogregressioninf_WriteReg(DDR_WEIGHTS_BASE_ADDR, i*4, ival);
+        printf( " 0x%x ", ival);
     }
 
-    // for (int i = 0 ; i < NumFeatures ; ++i)
-    // {
-    //     uint32_t data32 = 0;
-    //     float    floatValue;
-
-    //     data32 = XLogregressioninf_ReadReg(DDR_WEIGHTS_BASE_ADDR, i*4);
-    //     memcpy(&floatValue, &data32, sizeof(float));
-    //     printf("weights32: %f\n", floatValue);
-
-    // }
-
     xil_printf("Finished writing weights.\n");
+
+    // Read back and verify
+    xil_printf("Reading back weights from DDR for verification:\n");
+
+    for (int i = 0; i < numElements; ++i)
+    {
+        float fval;
+        int32_t val = XLogregressioninf_ReadReg(DDR_WEIGHTS_BASE_ADDR, i*4);
+        xil_printf("Read: 0x%x \n",  val);
+    }
 
 }
 
 
-
 int main()
 {
-//    unsigned int  LoopCount = 0;
-//    unsigned long TickStart = 0;
-//    unsigned long TickStop = 0;
-//    unsigned long Delta = 0;
-//    XScuTimer	Timer;
-    
+   unsigned int  LoopCount = 0;
+   unsigned long TickStart = 0;
+   unsigned long TickStop = 0;
+   unsigned long Delta = 0;
+   XScuTimer	Timer;
+
+   int numTestData = sizeof(testData) / sizeof(testData[0]);
+
     init_platform();
 
     LogRegressionInfInit(&LogRegInst, &Cfg);
 
-//#if 0
     // Set start address for data
     XLogregressioninf_Set_DataInBuff(&LogRegInst, DDR_DATA_BASE_ADDR_IN);
 
     // Set start address for weights
     XLogregressioninf_Set_WeightsBuff(&LogRegInst, DDR_WEIGHTS_BASE_ADDR);
 
-    //Set start address for prdictions
+    //Set start address for predictions
     XLogregressioninf_Set_PredictBuff(&LogRegInst, DDR_PREDICT_BASE_ADDR);
 
     WriteDataToBus();
     WriteWeightsToBus();
 
-    u32 iBias;
+    int32_t iBias;
     memcpy(&iBias, &Bias, sizeof(float));
     XLogregressioninf_Set_BiasP(&LogRegInst, iBias);
+    xil_printf("Bias = 0x%x \n", iBias);
 
-    xil_printf("Bias = %f \n", iBias);
-
-    XLogregressioninf_Set_DataDimensionP(&LogRegInst,4);
-    XLogregressioninf_Set_NumSamplesP(&LogRegInst,10);
-
-    //XLogregressioninf_EnableAutoRestart(&LogRegInst);
+    XLogregressioninf_Set_DataDimensionP(&LogRegInst,NUM_FEATURES);
+    XLogregressioninf_Set_NumSamplesP(&LogRegInst,NUM_SAMPLES);
 
     XLogregressioninf_Start(&LogRegInst);
 
     while(!XLogregressioninf_IsDone(&LogRegInst)){}
-    //sleep(2);
     printf("Got response !!!!!!!!!!!!\n");
-
+    
     // XScuTimer_Start(&Timer);
     // TickStart = XScuTimer_GetCounterValue(&Timer);
 
-    //while(!XLogregressioninf_IsDone(&LogRegInst))
-    // while(++LoopCount < 1000000)
-    // {
-    //     // TickStop = XScuTimer_GetCounterValue(&Timer);
-    //     // Delta = TickStop - TickStart;
-    //     // if (((double)(Delta)/XPAR_PS7_CORTEXA9_0_CPU_CLK_FREQ_HZ) > 1)
-    //     // {
-    //     //     break;
-    //     // }
-    // }
-
-    //sleep(2);
-
-    //uint32_t bias = XLogregressioninf_Get_PredictionsBuff(&LogRegInst);
-    //uint32_t cost = XLogregressioninf_Get_costP(&LogRegInst);
-
-    for (int i = 0 ; i < 10 ; ++i)
+    xil_printf("Read Predictions: ");
+    int32_t ival;
+    for (int i = 0; i < NUM_SAMPLES; ++i)
     {
-        uint32_t data32 = XLogregressioninf_ReadReg(DDR_PREDICT_BASE_ADDR, i*4);
-        float floatValue;
-        memcpy(&floatValue, &data32, sizeof(float));
-        xil_printf("p%d = 0x%04x \n", i, data32);
+        int32_t val = XLogregressioninf_ReadReg(DDR_PREDICT_BASE_ADDR, i*4);
+
+        xil_printf(" 0x%x ",  val);
     }
 
-    // for (int i = 0 ; i < 4 ; ++i)
-    // {
-    //     uint32_t data32 = XLogregressioninf_ReadReg(DDR_WEIGHTS_BASE_ADDR, i*4);
-    //     float floatValue;
-    //     memcpy(&floatValue, &data32, sizeof(float));
-    //     printf("w%d = %f \n", i, floatValue);
-    // }
-
-//    float fcost = (float)cost;
-//
-//    printf("Cost = %f \n", fcost);
-
-//#endif
-
-    print("Hello World\n\r");
-    print("Successfully ran Hello World application\n");
+    print("Successfully completed logistic inference application\n");
     cleanup_platform();
     return 0;
 }
