7af544d09e19ae0b714291fd682dafe229c39335
terenceza <158005283+terenceza@users.noreply.github.com>
2025-04-30 17:37:55 -0600

    Update LogRegressionHost.hpp

--- LogRegressionHost.hpp
+++ LogRegressionHost.hpp
@@ -1,6 +1,19 @@
+/******************************************************************************
+* Sensecoding.com free license
+* Run on Vitis 
+* Weights and Bias were generated with LogRegressionLearning 
+******************************************************************************/
+
 #ifndef LOG_REG__H
 #define LOG_REG__H
 
+#define MAX_LINE_LENGTH 100
+#define DDR_DATA_BASE_ADDR_IN       0x0000000000000101800
+#define DDR_PREDICT_BASE_ADDR       0x0000000000000101C00
+#define DDR_WEIGHTS_BASE_ADDR       0x0000000000000101D00
+
+#define NUM_SAMPLES 10
+#define NUM_FEATURES 4
 
 float testData[] = 
 {
@@ -16,24 +29,12 @@ float testData[] =
     5.9, 3.0, 5.1, 1.8
 };
 
-int testDataInt[] =
-{
-    6, 3, 5, 2,
-    6, 3, 5, 2,
-    5, 2, 5, 1,
-    6, 3, 5, 2,
-    6, 3, 5, 2,
-    6, 3, 5, 2,
-    6, 2, 5, 1,
-    6, 3, 5, 2,
-    6, 3, 5, 2,
-    5, 3, 5, 1
-};
-
 float Weights[] = 
 {
     -0.562098, -0.596679, 1.066087, 0.882578
 };
+
+float Predictions[10] = { 1.1, 1.2, 1.3, 1.4, 1.5, 1.1, 1.2, 1.3, 1.4, 1.5};
 
 float Bias = -0.087511;
 
