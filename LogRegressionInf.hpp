af7b9f29e7d8bc8b09c8eb5e88d67a463a6e8e91
terenceza <158005283+terenceza@users.noreply.github.com>
2025-04-30 17:42:13 -0600

    Update LogRegressionInf.hpp

--- LogRegressionInf.hpp
+++ LogRegressionInf.hpp
@@ -1,5 +1,5 @@
-#ifndef AUTOENCODER_HPP
-#define AUTOENCODER_HPP
+#ifndef LOG_REG_ING_HPP
+#define LOG_REG_ING_HPP
 
 #include <ap_fixed.h>
 
@@ -14,25 +14,24 @@
 #define MAX_EPOCHS       100
 
 #ifdef __SYNTHESIS__
-	typedef ap_fixed<25, 5, AP_RND_ZERO> DataType, CoeffType;
+	typedef ap_fixed<25, 5, AP_RND_ZERO> DataType;
 #else
 	typedef float DataType;
 	typedef double CoeffType;
 #endif
 
 void LogRegressionInf(int* DataInBuff,  int* PredictBuff,
-					  int* WeightsBuff ,int* BiasP,
-					  unsigned int* DataDimensionP, unsigned int* NumSamplesP);
+		      int* WeightsBuff ,int* BiasP,
+		      unsigned int* DataDimensionP, unsigned int* NumSamplesP);
 
 void Predict(   DataType* Inputs, DataType* Predictions,
-		 	 	DataType* Weights, DataType Bias,
-				unsigned int NumFeatures, unsigned int NumSamples);
+		DataType* Weights, DataType Bias,
+		unsigned int NumFeatures, unsigned int NumSamples);
 
 DataType sigmoid(DataType d);
 DataType Relu(DataType x);
 
 void CopyIntToDataTypeBuffers(int* From, DataType* To, unsigned int Dim);
 void CopyDataTypeToIntBuffers(DataType* From, int* To, unsigned int Dim);
-
 
 #endif
